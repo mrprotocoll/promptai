667de4aa85f129cada96558df56f8ae428ec6024
mrprotocoll <lekanvgbg@gmail.com>
2023-05-21 11:28:25 +0100

    Add root layout

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css'
+
+export const metadata = {
+    title: "Promptai",
+    description: "Discover and share AI prompts"
+}
+
+const RootLayout = ({children}) => {
+    return (
+        <html lang='en'>
+            <body>
+                <div className="main">
+                    <div className="gradient"></div>
+                </div>
+
+                <main className="app">
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
