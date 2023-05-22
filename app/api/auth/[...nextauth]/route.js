c01941e304a3d1e6588480ffca955585074847fa
mrprotocoll <lekanvgbg@gmail.com>
2023-05-22 19:30:58 +0100

    Add google authentication provider

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // ...add more providers here
+  ],
+
+  async session ({ session }) {
+
+  },
+
+  async signIn ({ profile }) {
+
+}
+})
+
+export { handler as GET, handler as POST }
