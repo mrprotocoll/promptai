394e202323776960f2d6bfd0b33ac33d350f466e
mrprotocoll <lekanvgbg@gmail.com>
2023-05-28 14:10:45 +0100

    new branch

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const CreatePrompt = () => {
+    return (
+
+    )
+}
+
+export default CreatePrompt
