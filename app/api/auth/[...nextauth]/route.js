00a6c8578964c9e8a5dff29589bcbffb11bd7fa9
mrprotocoll <lekanvgbg@gmail.com>
2023-05-28 09:50:48 +0100

    Add signin to authentication api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { connectToDB } from "@helpers/database"
+import User from "@models/user";
 
 const handler = NextAuth({
   // Configure one or more authentication providers
@@ -16,8 +18,23 @@ const handler = NextAuth({
   },
 
   async signIn ({ profile }) {
-
-}
+    try {
+      await connectToDB();
+      // check if the user already exist
+      const userExists = await User.findOne({
+        email: profile.email
+      })
+      // create user if user doesnot already exist
+      if(!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ","").toLowerCase()
+        })
+      }
+    } catch (error) {
+      
+    }
+  }
 })
 
 export { handler as GET, handler as POST }
